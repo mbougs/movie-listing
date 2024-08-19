c57832ff14d1e1f3f627808db168b3fc8d2f19f8
mbougs <nkanamario@yahoo.com>
2024-08-20 00:38:59 +0100

    backbone for app still in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-
+import s from './style.module.css';
 
 function App() {
 
   return (
     <>
+    <div className={s.main_container}>
+      <div className={s.header}>
+        <div className="row">
+          <div className="col-4">
+            <div>LOGO</div>
+            <div>Subtitle</div>
+          </div>
+          <div className="col-md-12 col-lg-4">
+            <input style={{width: "100%"}} type="text" />
+          </div>
+        </div>
+      </div>
+      <div className={s.tv_show_detail}>Show Details</div>
+      <div className={s.recommended_tv_shows}>Recommended for You</div>
+    </div>
     </>
   )
 }
